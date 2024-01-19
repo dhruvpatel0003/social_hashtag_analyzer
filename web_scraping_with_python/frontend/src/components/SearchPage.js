aeac3d49913a80df4ad4cb69f649d422144bfe86
dhruvpatel0003 <dhruvpatel152002@gmail.com>
2024-01-19 16:11:45 +0530

    Connection established

--- web_scraping_with_python/frontend/src/components/SearchPage.js
+++ web_scraping_with_python/frontend/src/components/SearchPage.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 
 const SearchPage = () => {
-    const [searchTerm, setSearchTerm] = useState('');
 
-    const handleSearch = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [hashtagData, setHashtagData] = useState(null);
+
+    const handleSearchClick = () => {
+
+    };
+
+    const handleCategoryClick = (category) => {
         // Perform search based on the searchTerm
-        console.log('Performing search for:', searchTerm);
+        console.log('Category search', category);
     };
 
     const handleOnHashtagClick = (hashtag) => {
-        
-       
+        console.log('Hashtag search', hashtag);
+        fetch(`/api/get-hashtag?hashtag=${hashtag}`, { method: 'GET' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok, status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => setHashtagData(data))
+            .catch(error => console.error('Error during fetch:', error));
     };
 
     return (
@@ -22,12 +37,13 @@ const SearchPage = () => {
                     onChange={(e) => setSearchTerm(e.target.value)}
                     placeholder="Enter your search term"
                 />
-                <button onClick={handleSearch}>Search</button>
+                <button onClick={handleSearchClick}>Search</button>
             </div>
+            {!searchTerm && <p>Please enter term into the search box</p>}
             <div>
                 <h3>Predefined Hashtags:</h3>
                 <div>
-                    <button onClick={() => handleOnHashtagClick('#imVKohli')}>#imVKohli</button>
+                    <button onClick={() => handleOnHashtagClick('imVKholi')}>#imVKohli</button>
                     <button onClick={() => handleOnHashtagClick('#hashtag2')}>#hashtag2</button>
                     <button onClick={() => handleOnHashtagClick('#hashtag3')}>#hashtag3</button>
                     {/* Add more predefined hashtags here */}
@@ -51,6 +67,12 @@ const SearchPage = () => {
                     {/* Add more categories here */}
                 </ul>
             </div>
+            {hashtagData && (
+                <div>
+                    <h3>Hashtag Data:</h3>
+                    <pre>{JSON.stringify(hashtagData, null, 2)}</pre>
+                </div>
+            )}
         </div>
     );
 };
